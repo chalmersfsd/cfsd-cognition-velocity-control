6ce3f6df2cd3092d71da679f92a4af8260d46409
lowitz <lovem@student.chalmers.se>
2019-05-27 11:19:28 +0200

    Added unit tests

--- test/tests-logic-velocity.cpp
+++ test/tests-logic-velocity.cpp
@@ -22,7 +22,82 @@
 #include "opendlv-standard-message-set.hpp"
 
 #include "logic-velocity.hpp"
+#include <Eigen/Core>
 
-TEST_CASE("Temporary holder") {
-  REQUIRE(true);
+TEST_CASE("Empty path should not return inf or nan") {
+  Eigen::MatrixXf path;
+
+  bool useConstantSpeed = false;
+  float ayLimit = 5.0f;
+  float velocityLimit = 10.0f;
+  float decelerationLimit = 5.0f;
+  VelocityControl velocityControl(useConstantSpeed, ayLimit, velocityLimit, decelerationLimit);
+  velocityControl.setPath(path);
+
+  opendlv::proxy::GroundSpeedRequest gsr = velocityControl.step();
+  float speed = gsr.groundSpeed();
+
+  REQUIRE(std::isfinite(speed));
+}
+
+
+TEST_CASE("Straight line should return velocity limit") {
+  Eigen::MatrixXf path(4,2);
+
+  path << 1.0f, 2.0f,
+          2.0f, 3.0f,
+          3.0f, 4.0f,
+          4.0f, 5.0f;
+
+  bool useConstantSpeed = false;
+  float ayLimit = 5.0f;
+  float velocityLimit = 10.0f;
+  float decelerationLimit = 5.0f;
+  VelocityControl velocityControl(useConstantSpeed, ayLimit, velocityLimit, decelerationLimit);
+  velocityControl.setPath(path);
+
+  opendlv::proxy::GroundSpeedRequest gsr = velocityControl.step();
+  float speed = gsr.groundSpeed();
+
+  REQUIRE(speed == Approx(velocityLimit).epsilon(0.01f));
+}
+
+TEST_CASE("If fewer than 4 points path should return initial speed request value") {
+  Eigen::MatrixXf path(3,2);
+
+  path << 1.0f, 2.0f,
+          2.0f, 3.0f,
+          3.0f, 4.0f;
+
+  bool useConstantSpeed = false;
+  float ayLimit = 5.0f;
+  float velocityLimit = 10.0f;
+  float decelerationLimit = 5.0f;
+  VelocityControl velocityControl(useConstantSpeed, ayLimit, velocityLimit, decelerationLimit);
+  velocityControl.setPath(path);
+
+  opendlv::proxy::GroundSpeedRequest gsr = velocityControl.step();
+  float speed = gsr.groundSpeed();
+
+  REQUIRE(speed == Approx(0.0f).epsilon(0.01f));
+}
+
+TEST_CASE("Return velocityLimit if constant speed is used") {
+  Eigen::MatrixXf path(3,2);
+
+  path << 1.0f, -1.0f,
+          2.0f, -4.0f,
+          3.0f, -9.0f;
+
+  bool useConstantSpeed = true;
+  float ayLimit = 5.0f;
+  float velocityLimit = 10.0f;
+  float decelerationLimit = 5.0f;
+  VelocityControl velocityControl(useConstantSpeed, ayLimit, velocityLimit, decelerationLimit);
+  velocityControl.setPath(path);
+
+  opendlv::proxy::GroundSpeedRequest gsr = velocityControl.step();
+  float speed = gsr.groundSpeed();
+
+  REQUIRE(speed == Approx(velocityLimit).epsilon(0.01f));
 }
